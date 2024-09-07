10ddda0a1c90772653c207ab03718072126295f9
Heyxd12 <btsarmh3215@gmail.com>
2024-09-07 19:52:52 +0600

    Update uptime.js

--- scripts/uptime.js
+++ scripts/uptime.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "uptime",
     aliases: ["up", "upt"],
     version: "1.0",
-    author: "XyryllPanget",
+    author: "XyryllPangt",
     role: 0,
     shortDescription: {
       en: "Displays the uptime of the bot."
